cdf8ebc7629553662edd3701c08d12b51bd793af
Saicharan <vsaicharan1998@gmail.com>
2019-11-26 15:53:00 +0530

    Fixed carousel image responsive and countdown timer

--- static/js/animation.js
+++ static/js/animation.js
@@ -42,43 +42,77 @@ $(window).scroll(function() {
 });  
 
 
+var day;
+var sec;
+var ticker;
 
-// set the date we're counting down to
-var target_date = new Date('Jan, 31, 2020').getTime();
- 
-// variables for time units
-var days, hours, minutes, seconds;
- 
-// get tag element
-var countdown = document.getElementById('countdown');
- 
-// update the tag with id "countdown" every 1 second
-setInterval(function () {
- 
-    // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
-     
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-     
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-     
-    // format countdown string + set tag value
-    countdown.innerHTML = '<span class="days">' + days +  ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hrs</label></span> <span class="minutes">'
-    + minutes + ' <label>Min</label></span> <span class="seconds">' + seconds + ' <label>Sec</label></span>';  
- 
-}, 1000);
+function getSeconds() {
+	let now 			= new Date();
+	let next			= new Date(2020, 0, 13, 5, 30, 00, 00 );
+	
+	let current 	= now.getTime();
+	let nextTime	= next.getTime();
+	let diff			= parseInt((next - now)/1000);
+	
+	startTimer (diff);
+}
+
+function startTimer(secs) {
+	sec = parseInt(secs);
+	ticker = setInterval("tick()", 1000);
+	tick();
+}
+
+function tick() {
+	let secs = sec;
+	
+	if (secs > 0) {
+		sec--;
+	} else {
+		clearInterval(ticker);
+		getSeconds();
+	}
+
+	
+	let days = Math.floor(secs/86400);
+	secs %= 86400;
+	let hours = Math.floor(secs/3600);
+	secs %= 3600;
+	let mins = Math.floor(secs/60);
+	secs %= 60;
+	
+	
+	
+	$('#days').text(days);
+	$('#hours').text(((hours < 10) ? '0' : '') + hours);
+	$('#minutes').text(((mins < 10) ? '0' : '') + mins );
+	$('#seconds').text(((secs < 10) ? '0' : '') + secs );
+}
+
+function animate_top (){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+
+		$('#back-to-top').click(function () {
+			$('#back-to-top').tooltip('hide');
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+
+		$('#back-to-top').tooltip('show');
+}
+
 $(document).ready(function() {
-          CountDownTimer('01/01/2020 06:0 AM', 'countdown');
-  });
-
-
+	tick();
+    animate_top();
+})
 
 
 
